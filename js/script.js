bca51fb34569b615068af7441ccf3356bdc8176f
abuxTM <abuxtmk1@gmail.com>
2025-05-18 20:22:06 +0100

    Added home page

--- js/script.js
+++ js/script.js
@@ -1,33 +1,127 @@
-  
-  /* Aidans jQuery Code */ 
-  
-  $(document).ready(function () {
-    
-    $('.Confirmation').hide();
+class Card {
+  constructor(hero, title, description, link_to_about_section) {
+    this.hero = hero;
+    this.title = title;
+    this.description = description;
+    this.link_to_about_section = link_to_about_section;
+  }
+}
+
+class CardManager {
+  constructor() {
+    this.cards = [];
+  }
+
+  // Appends new card
+  addCard(card) {
+    this.cards.push(card);
+  }
+
+  // Removes a card based on its title
+  removeCard(title) {
+    this.cards = this.cards.filter(card => card.title !== title);
+  }
+
+  // Creates cards in HTML
+  conjureCards() {
+    let card_spawner = document.getElementById("card_spawner");
+    card_spawner.innerHTML = "";
+
+    this.cards.forEach(card => {
+      // Main
+      // ========
+      let body = document.createElement("div");
+      body.setAttribute("class", "card");
+
+      // Hero
+      // ========
+      let image = document.createElement("img");
+      image.setAttribute("src", card.hero);
+      body.appendChild(image);
+
+      // Info box
+      // ========
+      let info = document.createElement("div");
+      info.setAttribute("class", "card_info");
+      body.appendChild(info);
+
+      // Title
+      // ========
+      let title = document.createElement("h1");
+      title.innerHTML = card.title;
+      info.appendChild(title);
+
+      // Div box
+      // ========
+      let div = document.createElement("div");
+      info.appendChild(div);
+
+      // Desc
+      // ========
+      let description = document.createElement("p");
+      description.innerHTML = card.description;
+      div.appendChild(description);
+
+      // Button
+      // ========
+      // TODO: Make sure to use 'link_to_about_section' here to open specific booking section
+      let find_more_btn = document.createElement("button");
+      find_more_btn.innerHTML = "Find More";
+      div.appendChild(find_more_btn);
+
+      // Assemble
+      // ========
+      card_spawner.appendChild(body);
+    });
+  }
+}
+
+let cards = new CardManager();
+
+// TODO: Add more cards
+cards.addCard(new Card("../img/gaming.png", "Video games room", "Play games n stuff", ""));
+cards.addCard(new Card("../img/gaming.png", "Video games room", "Play games n stuff", ""));
+cards.addCard(new Card("../img/gaming.png", "Video games room", "Play games n stuff", ""));
+
+cards.conjureCards();
+
+/* Aidans jQuery Code */
+
+$(document).ready(function () {
+  // Show booking confirmation section
+  $('#show-confirmation').click(function () {
+    $('.Confirmation').show();
     $('.Cancellation').hide();
-
-    // Show booking confirmation section
-    $('#show-confirmation').click(function () {
-      $('.Confirmation').show();
-      $('.Cancellation').hide();
-    });
-
-    // Show booking cancellation section
-    $('#show-cancellation').click(function () {
-      $('.Cancellation').show();
-      $('.Confirmation').hide();
-    });
-
-    // Optional: Add logic inside the confirmation section
-    $('.cancel').click(function () {
-      $('.Confirmation').hide();
-      $('.Cancellation').show();
-    });
-
-    // Optional: Return to home logic
-    $('.home-link, .back-link').click(function () {
-      $('.Confirmation, .Cancellation').hide();
-      // Show home section if you have one
-      // $('.Home').show();
-    });
   });
+
+  // Show booking cancellation section
+  $('#show-cancellation').click(function () {
+    $('.Cancellation').show();
+    $('.Confirmation').hide();
+  });
+
+  // Optional: Add logic inside the confirmation section
+  $('.cancel').click(function () {
+    $('.Confirmation').hide();
+    $('.Cancellation').show();
+  });
+
+  // Optional: Return to home logic
+  $('.home-link, .back-link').click(function () {
+    $('.Confirmation, .Cancellation').hide();
+    $('#home_section').show();
+  });
+});
+
+$(window).on('scroll', function () {
+  let scroll_top = $(window).scrollTop();
+  let window_height = $(window).height();
+  let document_height = $(document).height();
+
+  // Check for EOF (End-Of-File)
+  if (scroll_top + window_height >= document_height - 1) {
+    $('#navigatron').fadeOut(100);
+  } else {
+    $('#navigatron').fadeIn(100);
+  }
+});
